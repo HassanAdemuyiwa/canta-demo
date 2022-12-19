70997fe70c7f1d8a6014ebee7d35e5adc1d2943d
hassanademuyiwa <ademuyiwahassan68@gmail.com>
2022-12-19 20:37:42 +0100

    inial commit

---
+++ components/Navbar.js
@@ -0,0 +1,6 @@
+import { Flex } from "@chakra-ui/react";
+const Navbar = () => {
+  return <Flex>This is nav bar</Flex>;
+};
+
+export default Navbar;
---
+++ pages/form.js
@@ -0,0 +1,6 @@
+import { Text } from "@chakra-ui/react";
+const form = () => {
+  return <Text>This is a form</Text>;
+};
+
+export default form;
